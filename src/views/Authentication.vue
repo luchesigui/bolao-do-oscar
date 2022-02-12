6d993cc92c0c6e51042f4a2a73694722373fb101
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-02-12 17:47:59 -0300

    feat: style auth page

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,8 +1,23 @@
 <template>
-  <h1>Por favor, digite o nome do seu usuário</h1>
-  <form v-on:submit.prevent="login">
-    <input type="text" v-model="username" />
-    <button type="submit">Entrar</button>
+  <form
+    v-on:submit.prevent="login"
+    class="h-full container flex flex-col justify-center px-2 text-center -translate-y-5"
+  >
+    <img src="../assets/oscar.svg" class="max-w-xs mx-auto mb-24" />
+    <h1 class="text-xl font-bold mb-2">Bolão do Oscar {{ currentYear }}</h1>
+    <p class="mb-6">Digite seu nome e faça suas apostas</p>
+    <input
+      class="h-10 w-9/12 px-4 mx-auto mt-5 rounded-full text-gray-900 placeholder-gray-400"
+      type="text"
+      v-model="username"
+      placeholder="Nome"
+    />
+    <button
+      type="submit"
+      class="border border-white rounded-full mt-5 mx-auto w-fit px-9 py-2"
+    >
+      Entrar
+    </button>
   </form>
 </template>
 
@@ -12,6 +27,7 @@ export default {
   emits: ["login"],
   data() {
     return {
+      currentYear: new Date().getFullYear(),
       username: "",
     };
   },
