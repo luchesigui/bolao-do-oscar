9bb42dbf2781fb87e32406d162eda8606c51cffc
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-02-13 12:09:53 -0300

    feat: create tailwind container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     fontFamily: {
       sans: ["Montserrat", "sans-serif"],
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "0",
+      },
+    },
     extend: {},
   },
   plugins: [],
