cd5722bd2d4ac25559a378be069ba4e226caa94f
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-03-02 22:40:06 -0300

    fix(table): ranking styles

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -7,16 +7,17 @@
       <div
         class="flex border"
         :class="{
-          'rounded-tl rounded-tr': index === 0,
+          'border-t-0': index !== 0,
+          'rounded-tl rounded-tr border-t': index === 0,
           'rounded-bl rounded-br': index === positions.length - 1,
         }"
         v-for="(position, index) in positions"
       >
-        <div class="px-2 min-w-[40px] text-center">
+        <div class="px-3 py-1 min-w-[40px] text-center">
           {{ position.position }}
         </div>
-        <div class="px-2 border-x flex-1">{{ position.user }}</div>
-        <div class="px-2">{{ position.points }} pontos</div>
+        <div class="px-3 py-1 border-x flex-1">{{ position.user }}</div>
+        <div class="px-3 py-1 min-w-[100px] text-center">{{ position.points }} {{ position.points <= 1 ? 'ponto' : 'pontos' }}</div>
       </div>
     </div>
   </div>
