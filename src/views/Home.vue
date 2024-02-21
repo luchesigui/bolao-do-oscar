92cd8cab0486e24542dc65000e8adb33c6430f65
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2024-02-21 11:21:08 -0300

    fix: improve style of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container flex items-center text-center flex-1">
-    <h1 class="text-2xl w-full">
-      Bem vindos! Selecione uma opção no menu acima.
-    </h1>
-    <p>
-      Essa tela ainda não existe pois não temos uma pessoa focada em UX no
-      momento, mas aqui viria tipo um dashboard com atalho para áreas e
-      indicação de filmes.
-    </p>
-    <p>Pedimos desculpas pelo transtorno.</p>
+    <div class="flex flex-col">
+      <h1 class="text-2xl w-full mb-5">
+        Bem vindos! Selecione uma opção no menu acima.
+      </h1>
+
+      <p class="max-w-[50%] mx-auto">
+        Essa tela ainda não existe pois não temos uma pessoa focada em UX no
+        momento, mas aqui viria tipo um dashboard com atalho para áreas e
+        indicação de filmes. <strong>Pedimos desculpas pelo transtorno.</strong>
+      </p>
+    </div>
   </div>
 </template>
