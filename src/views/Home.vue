369806b3cf780fe7c300c8d118a19263adf5f6dc
Guilherme Luchesi <Guilherme.Luchesi@corp.motion-ind.com>
2024-02-21 11:14:02 -0300

    chore: improve dashboard message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container flex items-center text-center flex-1">
-    <h1 class="text-2xl w-full">Bem vindos! Selecione uma opção.</h1>
+    <h1 class="text-2xl w-full">
+      Bem vindos! Selecione uma opção no menu acima.
+    </h1>
+    <p>
+      Essa tela ainda não existe pois não temos uma pessoa focada em UX no
+      momento, mas aqui viria tipo um dashboard com atalho para áreas e
+      indicação de filmes.
+    </p>
+    <p>Pedimos desculpas pelo transtorno.</p>
   </div>
 </template>
